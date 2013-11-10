178eb23b25780632449aabac57b10ccf0a6406c1
Kolja Markwardt <vanmar.development@gmail.com>
2013-11-10 20:19:06 +0100

    Guarding against network not being available in notepad

--- HoebApp-app/src/de/vanmar/android/hoebapp/NotepadActivity.java
+++ HoebApp-app/src/de/vanmar/android/hoebapp/NotepadActivity.java
@@ -15,6 +15,7 @@ import de.vanmar.android.hoebapp.bo.Account;
 import de.vanmar.android.hoebapp.bo.MediaDetails;
 import de.vanmar.android.hoebapp.service.LibraryService;
 import de.vanmar.android.hoebapp.service.LoginFailedException;
+import de.vanmar.android.hoebapp.service.NetworkNotAvailableException;
 import de.vanmar.android.hoebapp.service.TechnicalException;
 import de.vanmar.android.hoebapp.util.NetworkHelper;
 import de.vanmar.android.hoebapp.util.Preferences_;
@@ -25,156 +26,164 @@ import java.util.List;
 @OptionsMenu(R.menu.notepadmenu)
 public class NotepadActivity extends FragmentActivity {
 
-    @Pref
-    Preferences_ prefs;
+	@Pref
+	Preferences_ prefs;
 
-    @ViewById(R.id.adView)
-    AdView adView;
+	@ViewById(R.id.adView)
+	AdView adView;
 
-    @ViewById(R.id.notepadlist)
-    ListView notepadList;
+	@ViewById(R.id.notepadlist)
+	ListView notepadList;
 
-    @ViewById(R.id.titlebar)
-    TextView titlebar;
+	@ViewById(R.id.titlebar)
+	TextView titlebar;
 
-    @Bean
-    LibraryService libraryService;
+	@Bean
+	LibraryService libraryService;
 
-    @Bean
-    NetworkHelper networkHelper;
-    private ArrayAdapter<MediaDetails> notepadAdapter;
-    private List<Account> accounts;
+	@Bean
+	NetworkHelper networkHelper;
 
-    @Override
-    protected void onCreate(final Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	private ArrayAdapter<MediaDetails> notepadAdapter;
+	private List<Account> accounts;
 
-        loadNotepad();
-    }
+	@Override
+	protected void onCreate(final Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-    @Override
-    protected void onResume() {
-        super.onResume();
+		loadNotepad();
+	}
 
-        final List<Account> accountPreferences = Account.fromString(prefs
-                .accounts().get());
-        if (accounts == null || !accounts.equals(accountPreferences)) {
-            this.accounts = accountPreferences;
-        }
-    }
+	@Override
+	protected void onResume() {
+		super.onResume();
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        // Another activity is taking focus (this activity is about to be
-        // "paused").
-    }
+		final List<Account> accountPreferences = Account.fromString(prefs
+				.accounts().get());
+		if (accounts == null || !accounts.equals(accountPreferences)) {
+			this.accounts = accountPreferences;
+		}
+	}
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-    }
+	@Override
+	protected void onPause() {
+		super.onPause();
+		// Another activity is taking focus (this activity is about to be
+		// "paused").
+	}
 
-    @OptionsItem(R.id.refresh)
-    void doRefresh() {
-        loadNotepad();
-    }
+	@Override
+	protected void onStop() {
+		super.onStop();
+	}
 
-    @AfterViews
-    void setupListAdapter() {
-        notepadAdapter = new ArrayAdapter<MediaDetails>(this,
-                R.layout.notepad_item) {
-            @Override
-            public View getView(final int position, final View convertView,
-                                final ViewGroup parent) {
-                final View view = getLayoutInflater().inflate(
-                        R.layout.notepad_item, null);
-                final MediaDetails item = getItem(position);
-                ((TextView) view.findViewById(R.id.author)).setText(item
-                        .getAuthor());
-                ((TextView) view.findViewById(R.id.title)).setText(item
-                        .getTitle());
-                ((TextView) view.findViewById(R.id.type)).setText(item
-                        .getType());
-                ((TextView) view.findViewById(R.id.signature)).setText(item
-                        .getSignature());
+	@OptionsItem(R.id.refresh)
+	void doRefresh() {
+		loadNotepad();
+	}
 
-                final LinearLayout layout = (LinearLayout) view;
-                layout.setBackgroundResource(item.getOwner().getAppearance().getDrawable());
+	@AfterViews
+	void setupListAdapter() {
+		notepadAdapter = new ArrayAdapter<MediaDetails>(this,
+				R.layout.notepad_item) {
+			@Override
+			public View getView(final int position, final View convertView,
+								final ViewGroup parent) {
+				final View view = getLayoutInflater().inflate(
+						R.layout.notepad_item, null);
+				final MediaDetails item = getItem(position);
+				((TextView) view.findViewById(R.id.author)).setText(item
+						.getAuthor());
+				((TextView) view.findViewById(R.id.title)).setText(item
+						.getTitle());
+				((TextView) view.findViewById(R.id.type)).setText(item
+						.getType());
+				((TextView) view.findViewById(R.id.signature)).setText(item
+						.getSignature());
 
-                view.setOnClickListener(new OnClickListener() {
+				final LinearLayout layout = (LinearLayout) view;
+				layout.setBackgroundResource(item.getOwner().getAppearance().getDrawable());
 
-                    @Override
-                    public void onClick(final View v) {
-                        final Intent intent = new Intent(NotepadActivity.this,
-                                DetailActivity_.class);
-                        intent.putExtra(DetailActivity.EXTRA_MEDIUM_ID,
-                                item.getId());
-                        startActivity(intent);
-                    }
-                });
-                view.findViewById(R.id.removeFromNotepad).setOnClickListener(new OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
+				view.setOnClickListener(new OnClickListener() {
+
+					@Override
+					public void onClick(final View v) {
+						final Intent intent = new Intent(NotepadActivity.this,
+								DetailActivity_.class);
+						intent.putExtra(DetailActivity.EXTRA_MEDIUM_ID,
+								item.getId());
+						startActivity(intent);
+					}
+				});
+				view.findViewById(R.id.removeFromNotepad).setOnClickListener(new OnClickListener() {
+					@Override
+					public void onClick(View v) {
 						notepadAdapter.remove(item);
-                        removeFromNotepad(item);
-                    }
-                });
+						removeFromNotepad(item);
+					}
+				});
 
-                return view;
-            }
+				return view;
+			}
 
-        };
-        notepadList.setAdapter(notepadAdapter);
-    }
+		};
+		notepadList.setAdapter(notepadAdapter);
+	}
 
 
-    @Background
-    void removeFromNotepad(MediaDetails mediaDetails) {
-        try {
-            libraryService.removeFromNotepad(mediaDetails.getOwner(), mediaDetails.getId());
-        } catch (TechnicalException e) {
-            displayError(e);
-        }
-    }
+	@Background
+	void removeFromNotepad(MediaDetails mediaDetails) {
+		try {
+			libraryService.removeFromNotepad(mediaDetails.getOwner(), mediaDetails.getId());
+		} catch (TechnicalException e) {
+			displayError(e);
+		}
+	}
 
-    @Background
-    void loadNotepad() {
-        try {
-            displayInTitle(getString(R.string.pleaseWait));
-            final List<MediaDetails> searchMedia = libraryService.loadNotepad();
-            displaySearchResults(searchMedia);
-            displayInTitle("");
-        } catch (final Exception e) {
-            displayError(e);
-        }
-    }
+	@Background
+	void loadNotepad() {
+		try {
+			if (!networkHelper.networkAvailable()) {
+				throw new NetworkNotAvailableException();
+			}
+			displayInTitle(getString(R.string.pleaseWait));
+			final List<MediaDetails> searchMedia = libraryService.loadNotepad();
+			displaySearchResults(searchMedia);
+		} catch (final Exception e) {
+			displayError(e);
+		} finally {
+			displayInTitle("");
+		}
+	}
 
-    @UiThread
-    void displayInTitle(final String text) {
-        titlebar.setText(text);
-    }
+	@UiThread
+	void displayInTitle(final String text) {
+		titlebar.setText(text);
+	}
 
-    @UiThread
-    void displaySearchResults(final List<MediaDetails> searchMedia) {
-        notepadAdapter.clear();
-        for (final MediaDetails item : searchMedia) {
-            notepadAdapter.add(item);
-        }
-    }
+	@UiThread
+	void displaySearchResults(final List<MediaDetails> searchMedia) {
+		notepadAdapter.clear();
+		for (final MediaDetails item : searchMedia) {
+			notepadAdapter.add(item);
+		}
+	}
 
-    @UiThread
-    void displayError(final Exception exception) {
-        exception.printStackTrace();
-        if (exception instanceof LoginFailedException) {
-            Toast.makeText(this, R.string.loginfailed, Toast.LENGTH_SHORT)
-                    .show();
-            Log.w(getClass().getCanonicalName(), "LoginFailedException");
-        } else if (exception instanceof TechnicalException) {
-            Toast.makeText(this, R.string.technicalError, Toast.LENGTH_SHORT)
-                    .show();
-            Log.e(getClass().getCanonicalName(), "TechnicalException: "
-                    + exception.getClass() + exception.getMessage());
-        }
-    }
+	@UiThread
+	void displayError(final Exception exception) {
+		exception.printStackTrace();
+		if (exception instanceof LoginFailedException) {
+			Toast.makeText(this, R.string.loginfailed, Toast.LENGTH_SHORT)
+					.show();
+			Log.w(getClass().getCanonicalName(), "LoginFailedException");
+		} else if (exception instanceof NetworkNotAvailableException) {
+			Toast.makeText(this, R.string.networkNotAvailable, Toast.LENGTH_SHORT)
+					.show();
+		} else if (exception instanceof TechnicalException) {
+			Toast.makeText(this, R.string.technicalError, Toast.LENGTH_SHORT)
+					.show();
+			Log.e(getClass().getCanonicalName(), "TechnicalException: "
+					+ exception.getClass() + exception.getMessage());
+		}
+	}
 }
---
+++ HoebApp-app/src/de/vanmar/android/hoebapp/service/NetworkNotAvailableException.java
@@ -0,0 +1,10 @@
+package de.vanmar.android.hoebapp.service;
+
+public class NetworkNotAvailableException extends Exception {
+
+	private static final long serialVersionUID = 1119080653454935673L;
+
+	public NetworkNotAvailableException() {
+		super();
+	}
+}
